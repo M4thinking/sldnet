940b2441423180ceb2436dc2c808268cfc027232
M4thinking <sebastianguzman@ug.uchile.cl>
2026-04-07 20:04:02 -0400

    feat: Refactor dataset loading and specifications into a structured format

--- dimmad_cli.py
+++ dimmad_cli.py
@@ -1,24 +1,26 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-import seaborn as sns
 import json
 import os
-
-from dimmad_benchmark.sbgm import SBGMAnomalyDetector
-from dimmad_benchmark import utils, alerceanomalies, run_experiments
-import matplotlib.gridspec as gridspec
-from tqdm.auto import tqdm
+import pickle
 import warnings
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from distclassipy.anomaly import DistanceAnomaly
+from fastparquet import ParquetFile
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import OneClassSVM
+from tqdm.auto import tqdm
+
+from dimmad_benchmark import alerceanomalies, run_experiments, utils
+from dimmad_benchmark.sbgm import SBGMAnomalyDetector
+
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-from sklearn.svm import OneClassSVM
-import distclassipy as dcpy
-from distclassipy.anomaly import DistanceAnomaly
-import pickle
-
-from sklearn.base import BaseEstimator, OutlierMixin
 epsilon = np.finfo(np.float32).eps
 
 
@@ -29,10 +31,6 @@ np.random.seed(seed_val)
 sns_dict = settings_dict["sns_dict"]
 sns.set_theme(**sns_dict)
 
-from IPython.core.display import HTML
-from IPython.display import display
-
-import squarify
 set3colors = [matplotlib.colors.rgb2hex(x) for x in plt.cm.Set3.colors]
 knowncolors = set3colors[:4]
 unknowncolors = ['#e6194B', '#3cb44b', '#ffe119',
@@ -43,13 +41,6 @@ unknowncolors = ['#e6194B', '#3cb44b', '#ffe119',
                  '#aaffc3', '#808000', '#ffd8b1',
                  '#000075', '#a9a9a9']
 unknowncolors = set3colors[4:]+unknowncolors
-
-from sklearn.ensemble import IsolationForest
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.preprocessing import StandardScaler
-
-from fastparquet import ParquetFile
 
 
 def _print_dataset_stats(features_df, new_knowns, new_unknowns, features_to_use):
@@ -67,181 +58,88 @@ def _print_dataset_stats(features_df, new_knowns, new_unknowns, features_to_use)
     normal_frac_withoutnans = np.median(normal_frac_withoutnans)
     print(f"The median no. of objects across columns which are not NaNs is {normal_frac_withoutnans:.1%}")
 
-## ELAsTiCC OOD
-def elasticc_ood(result_path):
-    elasticc_features = ParquetFile("data/dimmad/elasticc_features.parquet").to_pandas(index=False).set_index("snid")
-    
-    new_knowns = ['EB', 'DSCT', 'RRL', 'CEP']
-    new_unknowns = ['PISN-STELLA_HYDROGENIC', 'PISN-MOSFIT', 'uLens-Single_PyLIMA',
-        'TDE', 'SNIcBL+HostXT_V19', 'KN_B19', 'uLens-Binary', 'SL-SNII',
-        'SNIc-Templates', 'SLSN-I+host', 'SNIa-SALT3', 'SNIb-Templates',
-        'SNII+HostXT_V19', 'SNIa-91bg', 'SL-SNIb', 'Mdwarf-flare', 'ILOT',
-        'KN_K17', 'CART', 'SNIIb+HostXT_V19', 'SNIb+HostXT_V19', 'SL-SN1a',
-        'SNII-NMF', 'SNIIn+HostXT_V19', 'SNII-Templates',
-        'SNIc+HostXT_V19', 'SNIax', 'SNIIn-MOSFIT', 'uLens-Single-GenLens',
-        'PISN-STELLA_HECORE', 'AGN', 'SLSN-I_no_host', 'SL-SNIc',
-        'dwarf-nova']
+ELASTICC_FEATURES = [
+    'SPM_A_g', 'SPM_gamma_g', 'SPM_beta_g', 'SPM_tau_rise_g', 'SPM_tau_fall_g',
+    'SPM_A_r', 'SPM_gamma_r', 'SPM_beta_r', 'SPM_tau_rise_r', 'SPM_tau_fall_r',
+    'SPM_A_i', 'SPM_gamma_i', 'SPM_beta_i', 'SPM_tau_rise_i', 'SPM_tau_fall_i',
+    'SPM_A_z', 'SPM_gamma_z', 'SPM_beta_z', 'SPM_tau_rise_z', 'SPM_tau_fall_z',
+    'SPM_A_Y', 'SPM_gamma_Y', 'SPM_beta_Y', 'SPM_tau_rise_Y', 'SPM_tau_fall_Y',
+]
 
-    features_to_use = [
-        'SPM_A_g', 'SPM_gamma_g', 'SPM_beta_g', 'SPM_tau_rise_g', 'SPM_tau_fall_g',
-        'SPM_A_r', 'SPM_gamma_r', 'SPM_beta_r', 'SPM_tau_rise_r', 'SPM_tau_fall_r',
-        'SPM_A_i', 'SPM_gamma_i', 'SPM_beta_i', 'SPM_tau_rise_i', 'SPM_tau_fall_i',
-        'SPM_A_z', 'SPM_gamma_z', 'SPM_beta_z', 'SPM_tau_rise_z', 'SPM_tau_fall_z',
-        'SPM_A_Y', 'SPM_gamma_Y', 'SPM_beta_Y', 'SPM_tau_rise_Y', 'SPM_tau_fall_Y',
-    ]
+ZTF_FEATURES = [
+    "SPM_A_1", "SPM_t0_1", "SPM_gamma_1", "SPM_beta_1", "SPM_tau_rise_1", "SPM_tau_fall_1", "SPM_chi_1",
+    "SPM_A_2", "SPM_t0_2", "SPM_gamma_2", "SPM_beta_2", "SPM_tau_rise_2", "SPM_tau_fall_2", "SPM_chi_2",
+]
 
-    elasticc_features=elasticc_features[elasticc_features["class"].isin(new_knowns+new_unknowns)]
-
-    # Could use features we know are better for the 4 variable star classification
-    # but don't want to bias. additionally, the above features are simple and general
-    # and have very little NaNs.
-    _print_dataset_stats(elasticc_features, new_knowns, new_unknowns, features_to_use)
-
-    all_classes = np.concatenate([new_knowns, new_unknowns])
-    full_df = elasticc_features[elasticc_features['class'].isin(all_classes)]
-    full_df = full_df.loc[:, list(features_to_use) + ["class"]].dropna(subset=features_to_use)
-    full_df = full_df[~full_df.index.duplicated(keep='first')]
-
-    # full_df['status'] = np.where(full_df['class'].isin(new_knowns), 'normal', 'anomalous')
-    # will use this for stratification
-
-    full_inlier_df = full_df[full_df["class"].isin(new_knowns)]
-
-    full_outlier_df = full_df[full_df["class"].isin(new_unknowns)]
-    
-    return full_inlier_df, full_outlier_df, new_knowns, features_to_use
-
-
-## ELAsTiCC RID
-def elasticc_rid(result_path):
-    elasticc_features = ParquetFile("data/dimmad/elasticc_features.parquet").to_pandas(index=False).set_index("snid")
-    new_knowns = ['SNIcBL+HostXT_V19', 'SNIc-Templates', 'SNIa-SALT3', 'SNIb-Templates',
-             'SNII+HostXT_V19', 'SNIa-91bg', 'SNIIb+HostXT_V19', 'SNIb+HostXT_V19', 
-              'SNII-NMF', 'SNIIn+HostXT_V19', 'SNII-Templates','SNIc+HostXT_V19', 'SNIax', 
-              'SNIIn-MOSFIT', 'SLSN-I+host', 'SLSN-I_no_host']
-
-    new_unknowns = [
-        'PISN-STELLA_HYDROGENIC', 'PISN-MOSFIT','PISN-STELLA_HECORE',   
-                    ]
-
-    features_to_use = [
-        'SPM_A_g', 'SPM_gamma_g', 'SPM_beta_g', 'SPM_tau_rise_g', 'SPM_tau_fall_g',
-        'SPM_A_r', 'SPM_gamma_r', 'SPM_beta_r', 'SPM_tau_rise_r', 'SPM_tau_fall_r',
-        'SPM_A_i', 'SPM_gamma_i', 'SPM_beta_i', 'SPM_tau_rise_i', 'SPM_tau_fall_i',
-        'SPM_A_z', 'SPM_gamma_z', 'SPM_beta_z', 'SPM_tau_rise_z', 'SPM_tau_fall_z',
-        'SPM_A_Y', 'SPM_gamma_Y', 'SPM_beta_Y', 'SPM_tau_rise_Y', 'SPM_tau_fall_Y',
-    ]
-
-    elasticc_features=elasticc_features[elasticc_features["class"].isin(new_knowns+new_unknowns)]
-
-    # Could use features we know are better for the 4 variable star classification
-    # but don't want to bias. additionally, the above features are simple and general
-    # and have very little NaNs.
-    _print_dataset_stats(elasticc_features, new_knowns, new_unknowns, features_to_use)
-    
-    all_classes = np.concatenate([new_knowns, new_unknowns])
-    full_df = elasticc_features[elasticc_features['class'].isin(all_classes)]
-    full_df = full_df.loc[:, list(features_to_use) + ["class"]].dropna(subset=features_to_use)
-    full_df = full_df[~full_df.index.duplicated(keep='first')]
-
-    # full_df['status'] = np.where(full_df['class'].isin(new_knowns), 'normal', 'anomalous')
-    # will use this for stratification
-
-    full_inlier_df = full_df[full_df["class"].isin(new_knowns)]
-    full_outlier_df = full_df[full_df["class"].isin(new_unknowns)]
-    
-    return full_inlier_df, full_outlier_df, new_knowns, features_to_use
+DATASET_SPECS = {
+    ("elasticc", "ood"): {
+        "path": "data/dimmad/elasticc_features.parquet",
+        "index_col": "snid",
+        "knowns": ['EB', 'DSCT', 'RRL', 'CEP'],
+        "unknowns": [
+            'PISN-STELLA_HYDROGENIC', 'PISN-MOSFIT', 'uLens-Single_PyLIMA',
+            'TDE', 'SNIcBL+HostXT_V19', 'KN_B19', 'uLens-Binary', 'SL-SNII',
+            'SNIc-Templates', 'SLSN-I+host', 'SNIa-SALT3', 'SNIb-Templates',
+            'SNII+HostXT_V19', 'SNIa-91bg', 'SL-SNIb', 'Mdwarf-flare', 'ILOT',
+            'KN_K17', 'CART', 'SNIIb+HostXT_V19', 'SNIb+HostXT_V19', 'SL-SN1a',
+            'SNII-NMF', 'SNIIn+HostXT_V19', 'SNII-Templates',
+            'SNIc+HostXT_V19', 'SNIax', 'SNIIn-MOSFIT', 'uLens-Single-GenLens',
+            'PISN-STELLA_HECORE', 'AGN', 'SLSN-I_no_host', 'SL-SNIc',
+            'dwarf-nova',
+        ],
+        "features": ELASTICC_FEATURES,
+    },
+    ("elasticc", "rid"): {
+        "path": "data/dimmad/elasticc_features.parquet",
+        "index_col": "snid",
+        "knowns": [
+            'SNIcBL+HostXT_V19', 'SNIc-Templates', 'SNIa-SALT3', 'SNIb-Templates',
+            'SNII+HostXT_V19', 'SNIa-91bg', 'SNIIb+HostXT_V19', 'SNIb+HostXT_V19',
+            'SNII-NMF', 'SNIIn+HostXT_V19', 'SNII-Templates', 'SNIc+HostXT_V19', 'SNIax',
+            'SNIIn-MOSFIT', 'SLSN-I+host', 'SLSN-I_no_host',
+        ],
+        "unknowns": ['PISN-STELLA_HYDROGENIC', 'PISN-MOSFIT', 'PISN-STELLA_HECORE'],
+        "features": ELASTICC_FEATURES,
+    },
+    ("ztf", "ood"): {
+        "path": "data/dimmad/alerceztf_features.parquet",
+        "index_col": "oid",
+        "knowns": ['RRL', 'LPV', 'E', 'QSO', 'YSO', 'AGN', 'CEP', 'Periodic-Other', 'DSCT', 'Blazar', 'CV/Nova'],
+        "unknowns": ['SNIa', "SLSN", 'SNIbc', 'SNII'],
+        "features": ZTF_FEATURES,
+    },
+    ("ztf", "rid"): {
+        "path": "data/dimmad/alerceztf_features.parquet",
+        "index_col": "oid",
+        "knowns": ['LPV', 'E', 'Periodic-Other', 'Blazar', 'CV/Nova'],
+        "unknowns": ['RRL', 'CEP', 'DSCT'],
+        "features": ZTF_FEATURES,
+    },
+}
 
 
-## ZTF-ALeRCE OOD
-def ztf_ood(result_path):
-    alerce_ztf_features = ParquetFile("data/dimmad/alerceztf_features.parquet").to_pandas(index=False).set_index("oid")
-    new_knowns = ['RRL', 'LPV', 'E', 'QSO', 'YSO',  'AGN', 'CEP',
-              'Periodic-Other', 'DSCT', 'Blazar', 'CV/Nova']
+def load_dataset_split(dataset, scheme):
+    spec = DATASET_SPECS[(dataset, scheme)]
+    features_df = ParquetFile(spec["path"]).to_pandas(index=False).set_index(spec["index_col"])
+    new_knowns = spec["knowns"]
+    new_unknowns = spec["unknowns"]
+    features_to_use = spec["features"]
 
-    new_unknowns = ['SNIa', "SLSN", 'SNIbc', 'SNII']
-
-    features_to_use = [
-    "SPM_A_1",
-    "SPM_t0_1",
-    "SPM_gamma_1",
-    "SPM_beta_1",
-    "SPM_tau_rise_1",
-    "SPM_tau_fall_1",
-    "SPM_chi_1",
-    "SPM_A_2",
-    "SPM_t0_2",
-    "SPM_gamma_2",
-    "SPM_beta_2",
-    "SPM_tau_rise_2",
-    "SPM_tau_fall_2",
-    "SPM_chi_2",
-    ]
-
-    alerce_ztf_features=alerce_ztf_features[alerce_ztf_features["class"].isin(new_knowns+new_unknowns)]
+    features_df = features_df[features_df["class"].isin(new_knowns + new_unknowns)]
 
     # Could use features we know are better for the 4 variable star classification
     # but don't want to bias. additionally, the above features are simple and general
     # and have very little NaNs.
-    _print_dataset_stats(alerce_ztf_features, new_knowns, new_unknowns, features_to_use)
+    _print_dataset_stats(features_df, new_knowns, new_unknowns, features_to_use)
 
-    all_classes = np.concatenate([new_knowns, new_unknowns])
-    full_df = alerce_ztf_features[alerce_ztf_features['class'].isin(all_classes)]
-    full_df = full_df.loc[:, list(features_to_use) + ["class"]].dropna(subset=features_to_use)
+    full_df = features_df.loc[:, features_to_use + ["class"]].dropna(subset=features_to_use)
     full_df = full_df[~full_df.index.duplicated(keep='first')]
 
     # full_df['status'] = np.where(full_df['class'].isin(new_knowns), 'normal', 'anomalous')
     # will use this for stratification
-
     full_inlier_df = full_df[full_df["class"].isin(new_knowns)]
-
     full_outlier_df = full_df[full_df["class"].isin(new_unknowns)]
-    
-    return full_inlier_df, full_outlier_df, new_knowns, features_to_use
 
-## ZTF-ALeRCE RID
-def ztf_rid(result_path):
-    alerce_ztf_features = ParquetFile("data/dimmad/alerceztf_features.parquet").to_pandas(index=False).set_index("oid")
-    new_knowns = [ 'LPV', 'E', 'Periodic-Other', 'Blazar', 'CV/Nova']
-
-    new_unknowns = ['RRL','CEP', 'DSCT', ]
-
-    features_to_use = [
-    "SPM_A_1",
-    "SPM_t0_1",
-    "SPM_gamma_1",
-    "SPM_beta_1",
-    "SPM_tau_rise_1",
-    "SPM_tau_fall_1",
-    "SPM_chi_1",
-    "SPM_A_2",
-    "SPM_t0_2",
-    "SPM_gamma_2",
-    "SPM_beta_2",
-    "SPM_tau_rise_2",
-    "SPM_tau_fall_2",
-    "SPM_chi_2",
-    ]
-
-    alerce_ztf_features=alerce_ztf_features[alerce_ztf_features["class"].isin(new_knowns+new_unknowns)]
-
-    # Could use features we know are better for the 4 variable star classification
-    # but don't want to bias. additionally, the above features are simple and general
-    # and have very little NaNs.
-    _print_dataset_stats(alerce_ztf_features, new_knowns, new_unknowns, features_to_use)
-
-    all_classes = np.concatenate([new_knowns, new_unknowns])
-    full_df = alerce_ztf_features[alerce_ztf_features['class'].isin(all_classes)]
-    full_df = full_df.loc[:, list(features_to_use) + ["class"]].dropna(subset=features_to_use)
-    full_df = full_df[~full_df.index.duplicated(keep='first')]
-
-    # full_df['status'] = np.where(full_df['class'].isin(new_knowns), 'normal', 'anomalous')
-    # will use this for stratification
-
-    full_inlier_df = full_df[full_df["class"].isin(new_knowns)]
-
-    full_outlier_df = full_df[full_df["class"].isin(new_unknowns)]
-    
     return full_inlier_df, full_outlier_df, new_knowns, features_to_use
 
 def run(result_path, full_inlier_df, full_outlier_df, new_knowns, features_to_use, models_to_test, seed_val, num_runs=20):
@@ -313,16 +211,7 @@ if __name__ == "__main__":
     scheme = args.scheme
     epochs = args.epochs
     
-    if dataset=="elasticc" and scheme=="rid":
-        data_function = elasticc_rid
-    elif dataset=="elasticc" and scheme=="ood":
-        data_function = elasticc_ood
-    elif dataset=="ztf" and scheme=="ood":
-        data_function = ztf_ood
-    elif dataset=="ztf" and scheme=="rid":
-        data_function = ztf_rid
-        
-    full_inlier_df, full_outlier_df, new_knowns, features_to_use = data_function(result_path)
+    full_inlier_df, full_outlier_df, new_knowns, features_to_use = load_dataset_split(dataset, scheme)
     
     epochs = args.epochs
     
@@ -333,7 +222,6 @@ if __name__ == "__main__":
             epochs=epochs,
             L=4,
             betas=(0.9, 0.999),
-            sigma_high=1.0
         ),
         "SLDNet (L:0.001)": SBGMAnomalyDetector(
             epochs=epochs,
